440a66f5116f81ac60f3f068ffe5a68662e7d86b
Yeomdongsu <117874997+Yeomdongsu@users.noreply.github.com>
2024-06-04 21:56:27 +0900

    수정

--- apart_info.py
+++ apart_info.py
@@ -1,40 +1,39 @@
 from flask import request
 from flask_restful import Resource
 from config import Config
-
-import pymysql
-import pymysql.cursors
-from sshtunnel import SSHTunnelForwarder   
+from mysql_connection import get_connection   
 
 class ApartInfoResource(Resource) :
 
 	def get(self) :
 		# data = request.get_json()
 		
-		with SSHTunnelForwarder(Config.HOST, ssh_username=Config.USERNAME, ssh_password=Config.PASSWORD, remote_bind_address=('127.0.0.1', 3306)) as tunnel:
-			try :
-				conn = pymysql.connect(host="127.0.0.1", user=Config.USERNAME, password=Config.PASSWORD, database='test_ds', port=tunnel.local_bind_port, charset="utf8")
-					
-				cursor = conn.cursor(pymysql.cursors.DictCursor)
+		try :
+			conn = get_connection()
+				
+			cursor = conn.cursor(dictionary=True)
 
-				query = '''
-						SELECT * 
-						FROM apart
-						ORDER BY date desc
-						LIMIT 5; 
-						'''
+			query = '''
+					SELECT * 
+					FROM apart
+					ORDER BY id desc
+					LIMIT 5; 
+					'''
 
-				cursor.execute(query)
+			cursor.execute(query)
 
-				result_list = cursor.fetchall()
+			result_list = cursor.fetchall()
 
-			except Exception as e :
-				print(f"error : {str(e)}")
-				return {"result" : "Fail"}, 500
-			
-			finally :
-				if cursor : cursor.close()
-				if conn : conn.close()	
+			for i, row in enumerate(result_list) :
+				result_list[i]["createdAt"] = row["createdAt"].isoformat()
 
-		return {"result" : "success", "result_list" : result_list}, 200
+		except Exception as e :
+			print(f"error : {str(e)}")
+			return {"result" : "Fail"}, 500
+		
+		finally :
+			if cursor : cursor.close()
+			if conn : conn.close()	
+
+		return {"result" : "success", "result_list" : result_list, "count" : len(result_list)}, 200
     
--- main.py
+++ main.py
@@ -2,10 +2,7 @@ import requests
 from config import Config
 from file_reader_parser import read_file, parse_lines_to_dict
 import xml.etree.ElementTree as ET
-
-import pymysql
-import pymysql.cursors
-from sshtunnel import SSHTunnelForwarder   
+from mysql_connection import get_connection
 
 # 아파트 매매 정보(주소)로 위도 경도 구하는 함수(카카오 로컬 API) 
 def get_lat_lng(address, LatLngServiceKey):
@@ -46,67 +43,66 @@ root = ET.fromstring(xml_data.text)
 
 item_list = root.findall('.//item')
 
-with SSHTunnelForwarder(Config.HOST, ssh_username=Config.USERNAME, ssh_password=Config.PASSWORD, remote_bind_address=('127.0.0.1', 3306)) as tunnel:
-	try :
-		conn = pymysql.connect(host="127.0.0.1", user=Config.USERNAME, password=Config.PASSWORD, database='test_ds', port=tunnel.local_bind_port, charset="utf8")
-			
-		cursor = conn.cursor(pymysql.cursors.DictCursor)
+try :
+	conn = get_connection()		
+		
+	cursor = conn.cursor()
 
-		for item in item_list :
-			price = item.find("거래금액").text # 가격
-			transactionType = item.find("거래유형").text # ex) 중개거래
-			constructionYear = item.find("건축년도").text # 지어진 연도
-			year = item.find("년").text # 판매된 연도
-			# roadName = item.find("도로명").text # 도로명 ex) 아카데미로
-			buildingMainCode = item.find("도로명건물본번호코드").text # ex) 446 
-			buildingSubCode = item.find("도로명건물부번호코드").text # 나머지(부속건물이 있는 경우)
-			dong = item.find("동").text # 아파트 동 번호가 있는 경우
-			legalDong = item.find("법정동").text # ex) 송도동
-			apartment = item.find("아파트").text # 아파트명
-			month = item.find("월").text # 판매 날짜(월)
-			day = item.find("일").text # 판매 날짜(일)
-			area = item.find("전용면적").text # 면적
-			lotNumber = item.find("지번").text # ex) 397-8
-			regionCode = item.find("지역코드").text # 위 LAWD_CD와 같음
-			floor = item.find("층").text # 몇 층인지
+	for item in item_list :
+		price = item.find("거래금액").text # 가격
+		transactionType = item.find("거래유형").text # ex) 중개거래
+		constructionYear = item.find("건축년도").text # 지어진 연도
+		year = item.find("년").text # 판매된 연도
+		# roadName = item.find("도로명").text # 도로명 ex) 아카데미로
+		buildingMainCode = item.find("도로명건물본번호코드").text # ex) 446 
+		buildingSubCode = item.find("도로명건물부번호코드").text # 나머지(부속건물이 있는 경우)
+		dong = item.find("동").text # 아파트 동 번호가 있는 경우
+		legalDong = item.find("법정동").text # ex) 송도동
+		apartment = item.find("아파트").text # 아파트명
+		month = item.find("월").text # 판매 날짜(월)
+		day = item.find("일").text # 판매 날짜(일)
+		area = item.find("전용면적").text # 면적
+		lotNumber = item.find("지번").text # ex) 397-8
+		regionCode = item.find("지역코드").text # 위 LAWD_CD와 같음
+		floor = item.find("층").text # 몇 층인지
 
-			# 가공 작업
-			price = price.replace(",", "").replace("'", "").strip() + "0000"
-			legalDong = legalDong.strip()
-			regionCode = result_dict.get(regionCode, "알 수 없는 지역코드")
-			address = f"{regionCode} {legalDong} {lotNumber}"
+		# 가공 작업
+		price = price.replace(",", "").replace("'", "").strip() + "0000"
+		legalDong = legalDong.strip()
+		regionCode = result_dict.get(regionCode, "알 수 없는 지역코드")
+		address = f"{regionCode} {legalDong} {lotNumber}"
 
-			# 아파트 주소로 위도/경도 찾기
-			lat, lng = get_lat_lng(address, LatLngServiceKey)
-			
-			if lat is None and lng is None : 
-				print(f"{address} 주소로 위도 경도를 찾을 수 없습니다.")
-				continue
+		# 아파트 주소로 위도/경도 찾기
+		lat, lng = get_lat_lng(address, LatLngServiceKey)
+		
+		if lat is None and lng is None : 
+			print(f"{address} 주소로 위도 경도를 찾을 수 없습니다.")
+			continue
 
-			# if buildingSubCode == "00000" : roadName = f"{roadName} {buildingMainCode}"
-			# else : roadName = f"{roadName} {buildingMainCode}-{buildingSubCode}"	
-			
-			if dong != " " : 
-				print(f"{year}년 {month}월 {day}일 {address} [위도/경도 : {lat}, {lng}] 면적 {area}의 {dong}동 {apartment}아파트({floor}층)가 {price}원에 {transactionType}로 판매되었습니다. 건축년도 : {constructionYear}")
-			else :
-				print(f"{year}년 {month}월 {day}일 {address} [위도/경도 : {lat}, {lng}] 면적 {area}의 {apartment}아파트({floor}층)가 {price}원에 {transactionType}로 판매되었습니다. 건축년도 : {constructionYear}")
+		# if buildingSubCode == "00000" : roadName = f"{roadName} {buildingMainCode}"
+		# else : roadName = f"{roadName} {buildingMainCode}-{buildingSubCode}"	
+		
+		if dong != " " : 
+			print(f"{year}년 {month}월 {day}일 {address} [위도/경도 : {lat}, {lng}] 면적 {area}의 {dong}동 {apartment}아파트({floor}층)가 {price}원에 {transactionType}로 판매되었습니다. 건축년도 : {constructionYear}")
+		else :
+			print(f"{year}년 {month}월 {day}일 {address} [위도/경도 : {lat}, {lng}] 면적 {area}의 {apartment}아파트({floor}층)가 {price}원에 {transactionType}로 판매되었습니다. 건축년도 : {constructionYear}")
 
-			query = '''
-					INSERT IGNORE INTO apart 
-					(date, address, latitude, longitude, area, apartment, floor, price, transactionType, constructionYear) 
-					VALUES 
-					(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
-					'''
-			record = (f"{year}-{month}-{day}", address, lat, lng, area, apartment, floor, price, transactionType, constructionYear)
+		query = '''
+				INSERT IGNORE INTO apart 
+				(date, address, latitude, longitude, area, apartment, floor, price, transactionType, constructionYear) 
+				VALUES 
+				(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+				'''
+		record = (f"{year}-{month}-{day}", address, lat, lng, area, apartment, floor, price, transactionType, constructionYear)
 
-			cursor.execute(query, record)
+		cursor.execute(query, record)
 
-			conn.commit()
+		conn.commit()
 
-	except Exception as e :
-		print(f"error : {str(e)}")
-	
-	finally :
-		if cursor : cursor.close()
-		if conn : conn.close()	
+except Exception as e :
+	print(f"error : {str(e)}")
+
+finally :
+	if cursor : cursor.close()
+	if conn : conn.close()	
 
